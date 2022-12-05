d44aeaf2a708612910921e225f419b7e3530b324
McBowie <miloszchrachol@gmail.com>
2022-12-05 21:33:01 +0100

    Zombee physics added

--- Zombee-game-project/Assets/Zombee.cs
+++ Zombee-game-project/Assets/Zombee.cs
@@ -6,6 +6,7 @@ public class Zombee : MonoBehaviour
 {
     Transform wayPoint;
     Rigidbody2D rb;
+    [SerializeField]float walkingSpeed;
 
     private void Awake()
     {
@@ -21,7 +22,7 @@ public class Zombee : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(transform.forward * walkingSpeed, ForceMode2D.Impulse);
+        rb.AddForce(10 * Time.deltaTime * walkingSpeed * 220 * transform.right,ForceMode2D.Force);
         float angle = Mathf.Atan2(wayPoint.position.y - transform.position.y, wayPoint.position.x - transform.position.x) * Mathf.Rad2Deg;
         Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 100 * Time.deltaTime);
